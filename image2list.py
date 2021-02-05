349ae7e34f983cae1dc95f05b2559ee35279dda2
Xiaosea <maysrp@gmail.com>
2021-02-06 07:24:27 +0800

    Update image2list.py

--- image2list.py
+++ image2list.py
@@ -1,63 +1,32 @@
-# from machine import I2C,Pin
-# from ssd1306 import SSD1306_I2C#I2C的oled选该方法
-# i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000) 
-# oled = SSD1306_I2C(128, 64, i2c) #你的OLED分辨率，使用I2C
-# import ujson as json
-# oled.fill(1) #清空屏幕
-# oled.show()
-# oled.fill(0)
-# oled.show()
-
 import json
-import time
-import gc
+from PIL import Image
+from pathlib import Path
 
-def lifegame(past,xa=128,ya=64):
-    now=[]
-    decell={}
-    for i in past:
-        j=[]
-        j.append([i[0]-1,i[1]-1])
-        j.append([i[0],i[1]-1])
-        j.append([i[0]+1,i[1]-1])
-        j.append([i[0]-1,i[1]])
-        j.append([i[0]+1,i[1]])
-        j.append([i[0]-1,i[1]+1])
-        j.append([i[0],i[1]+1])
-        j.append([i[0]+1,i[1]+1])
-        jsj=0
-        for x in j:
-            if x in past:
-                jsj+=1
-            else:
-                if x[0] in range(xa) and x[1] in range(ya):
-                    xxx=json.dumps(x)
-                    if xxx in decell:
-                        decell[xxx]+=1
-                    else:
-                        decell[xxx]=1
-        if jsj in [2,3]:
-            now.append(i)
-    for dec in decell:
-        if decell[dec]==3:
-            now.append(json.loads(dec))
-    del past
-    del decell
-    gc.collect()
-    return now
+def pl(img,x=128,y=64,fr=False):
+    e=[]
+    fe=[]
+    c=Path(img)
+    if c.is_file():
+        d=Image.open(img)
+        q=d.convert("1")
+        q2=q.resize((x,y))
+        q2.show()
+        q3=q2.load()
+        for i in range(x):
+            for j in range(y):
+                if q3[i,j]>150:
+                    e.append([i,j])
+                else:
+                    fe.append([i,j])
+    f=open(c.stem+".json","w")
+    if fr:
+        f.write(json.dumps(fe))
+        f.close()
+        return e
+    else:
+        f.write(json.dumps(e))
+        f.close()
+        return e
 
-def showImage(li,oled):
-    oled.fill(0)
-    for i in li:
-        oled.pixel(i[0],i[1],1)
-    oled.show()
-
-f=open("lo.json",'r')
-d=json.loads(f.read())
-f.close()
-for i in range(100):
-    t=time.time()
-    d=lifegame(d)
-    print(i,":",len(d),":",time.time()-t,gc.mem_free())
-    showImage(d,oled)
-    gc.collect()
+q=pl("lo.png",32,32,True)
+print(len(q))
