20606034629b17c805ef8d0436c210ee89f30139
Xiaosea <maysrp@gmail.com>
2021-02-05 17:04:51 +0800

    Update image2list.py

--- image2list.py
+++ image2list.py
@@ -2,23 +2,30 @@ import json
 from PIL import Image
 from pathlib import Path
 
-def pl(img,x=128,y=64):
+def pl(img,x=128,y=64,p=False):
     e=[]
+    fs=[]
     c=Path(img)
     if c.is_file():
         d=Image.open(img)
         q=d.convert("1")
         q2=q.resize((x,y))
-        # q2.show()
+        q2.show()
         q3=q2.load()
         for i in range(x):
             for j in range(y):
                 if q3[i,j]>150:
                     e.append([i,j])
+                else:
+                    fs.append([i,j])
     f=open(c.stem+".json","w")
-    f.write(json.dumps(e))
-    f.close()
-    return e
+    if p:
+        f.write(json.dumps(fs))
+        return fs
+    else:
+        f.write(json.dumps(e))
+        f.close()
+        return e
 
-q=pl("a.jpg",32,16)
-print(q)
+q=pl("lo.png",32,32,True)
+print(len(q))
